68601e4e4a4366c373e5f98eb4086939441be3c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-23 23:57:00 +0000

    Deploy kerfootj/kerfootj.github.io to github.com/kerfootj/kerfootj.github.io.git:master

---
+++ precache-manifest.da63535a729d03dced33aee57f1f19da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d86958615733d633a1466cc81932d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64d908146e0e4a95f80b",
+    "url": "/static/css/main.0310c2ee.chunk.css"
+  },
+  {
+    "revision": "1fa3e45a8f6f0b8c5b94",
+    "url": "/static/js/2.eb58f25e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.eb58f25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d908146e0e4a95f80b",
+    "url": "/static/js/main.014e2d05.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
